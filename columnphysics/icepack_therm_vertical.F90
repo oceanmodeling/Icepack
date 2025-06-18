897639f7f8d24ad5c7b61f23595c905ef58c467a
Joseph Smith <smithj28@oregonstate.edu>
2025-06-18 12:43:18 -0500

    added fix for open BC to deal with small ice fraction crashing model.

--- columnphysics/icepack_therm_vertical.F90
+++ columnphysics/icepack_therm_vertical.F90
@@ -2731,10 +2731,21 @@
                                  prescribed_ice=prescribed_ice)
 
             if (icepack_warnings_aborted(subname)) then
-               write(warnstr,*) subname, ' ice: Vertical thermo error, cat ', n
-               call icepack_warnings_add(warnstr)
-               return
+               
+               if ((vicen(n)/aicen(n)) > real(5e-2)) then
+                  write(warnstr,*) subname, ' ice: Vertical thermo error, cat ', n
+                  call icepack_warnings_add(warnstr)
+                  return
+               else 
+                  call icepack_warnings_setabort(.false.,__FILE__,__LINE__)
+               endif
+               
+               !write(warnstr,*) subname, ' ice: Vertical thermo error, cat ', n
+               !call icepack_warnings_add(warnstr)
+               !return
+            
             endif
+            
 
             if (snwgrain) then
                rsnwn (:,n) = rsnw (:)
